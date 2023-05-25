60223a5efc629bda8a54f778bf69187362dfd5e4
ButakovaLudmila <ludmila.butakova@gmail.com>
2023-05-25 12:48:41 +0300

    yes3

---
+++ WFCS/ITMO_Group12405_WindowsForms_Lab1/Lab1_3/Program.cs
@@ -0,0 +1,25 @@
+﻿using ITMO_Group12405_WindowsForms_Lab1.Lab1_3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ITMO_Group12405_WindowsForms_Lab1.Lab1_3
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+
+        {
+            
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new nForm());
+        }
+    }
+}
