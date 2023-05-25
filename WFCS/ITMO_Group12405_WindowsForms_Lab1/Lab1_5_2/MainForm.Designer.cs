a5b090d6ebe41e41e2532bba29a14957ab47950a
ButakovaLudmila <ludmila.butakova@gmail.com>
2023-05-25 12:52:54 +0300

    yes4

---
+++ WFCS/ITMO_Group12405_WindowsForms_Lab1/Lab1_5_2/MainForm.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace ITMO_Group12405_WindowsForms_Lab1.Lab1_5_2
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Close = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Close
+            // 
+            this.Close.Location = new System.Drawing.Point(285, 174);
+            this.Close.Name = "Close";
+            this.Close.Size = new System.Drawing.Size(211, 99);
+            this.Close.TabIndex = 0;
+            this.Close.Text = "GREENPEACE";
+            this.Close.UseVisualStyleBackColor = true;
+            this.Close.Click += new System.EventHandler(this.Close_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Green;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Close);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Main Form";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Close;
+    }
+}
---
+++ WFCS/ITMO_Group12405_WindowsForms_Lab1/Lab1_5_2/SubForm.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ITMO_Group12405_WindowsForms_Lab1.Lab1_5_2
+{
+    public partial class SubForm : Form
+    {
+        public SubForm()
+        {
+            InitializeComponent();
+        }
+
+        private void SubForm_Load(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Maximized;
+
+        }
+
+        private void New_Click(object sender, EventArgs e)
+        {
+            MainForm newMain = new MainForm();
+            newMain.MdiParent = this;
+            newMain.Show();
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
