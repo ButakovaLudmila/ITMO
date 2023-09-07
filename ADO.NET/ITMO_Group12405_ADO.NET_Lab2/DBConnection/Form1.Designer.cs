c14073f2c86987a7e6281739997e6e4570b05610
ButakovaLudmila <ludmila.butakova@gmail.com>
2023-09-07 14:18:45 +0300

    Ado2

--- ADO.NET/ITMO_Group12405_ADO.NET_Lab2/DBConnection/Form1.Designer.cs
+++ ADO.NET/ITMO_Group12405_ADO.NET_Lab2/DBConnection/Form1.Designer.cs
@@ -29,10 +29,16 @@
         private void InitializeComponent()
         {
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.dBConnectionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.dBCloseConnectionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.списокПодключенийToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dBConnectionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dBCloseConnectionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button3 = new System.Windows.Forms.Button();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -50,20 +56,6 @@
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
-            // dBConnectionToolStripMenuItem
-            // 
-            this.dBConnectionToolStripMenuItem.Name = "dBConnectionToolStripMenuItem";
-            this.dBConnectionToolStripMenuItem.Size = new System.Drawing.Size(141, 29);
-            this.dBConnectionToolStripMenuItem.Text = "DBConnection";
-            this.dBConnectionToolStripMenuItem.Click += new System.EventHandler(this.dBConnectionToolStripMenuItem_Click);
-            // 
-            // dBCloseConnectionToolStripMenuItem
-            // 
-            this.dBCloseConnectionToolStripMenuItem.Name = "dBCloseConnectionToolStripMenuItem";
-            this.dBCloseConnectionToolStripMenuItem.Size = new System.Drawing.Size(184, 29);
-            this.dBCloseConnectionToolStripMenuItem.Text = "DBCloseConnection";
-            this.dBCloseConnectionToolStripMenuItem.Click += new System.EventHandler(this.dBCloseConnectionToolStripMenuItem_Click);
-            // 
             // файлToolStripMenuItem
             // 
             this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
@@ -79,11 +71,86 @@
             this.списокПодключенийToolStripMenuItem.Text = "Список подключений";
             this.списокПодключенийToolStripMenuItem.Click += new System.EventHandler(this.списокПодключенийToolStripMenuItem_Click);
             // 
+            // dBConnectionToolStripMenuItem
+            // 
+            this.dBConnectionToolStripMenuItem.Name = "dBConnectionToolStripMenuItem";
+            this.dBConnectionToolStripMenuItem.Size = new System.Drawing.Size(141, 29);
+            this.dBConnectionToolStripMenuItem.Text = "DBConnection";
+            this.dBConnectionToolStripMenuItem.Click += new System.EventHandler(this.dBConnectionToolStripMenuItem_Click);
+            // 
+            // dBCloseConnectionToolStripMenuItem
+            // 
+            this.dBCloseConnectionToolStripMenuItem.Name = "dBCloseConnectionToolStripMenuItem";
+            this.dBCloseConnectionToolStripMenuItem.Size = new System.Drawing.Size(184, 29);
+            this.dBCloseConnectionToolStripMenuItem.Text = "DBCloseConnection";
+            this.dBCloseConnectionToolStripMenuItem.Click += new System.EventHandler(this.dBCloseConnectionToolStripMenuItem_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(21, 49);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(174, 47);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Сколько продуктов";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(213, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(157, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Сколько продуктов";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(21, 112);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(174, 47);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Список продуктов";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(217, 112);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(178, 215);
+            this.listView1.TabIndex = 4;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Название продукта";
+            this.columnHeader1.Width = 175;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(21, 376);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(174, 47);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Транзакция";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // DBConnection
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "DBConnection";
@@ -103,6 +170,12 @@
         private System.Windows.Forms.ToolStripMenuItem dBCloseConnectionToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem списокПодключенийToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.Button button3;
     }
 }
 
